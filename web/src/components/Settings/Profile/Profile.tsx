a3d6f58442eff1895099ae6e4dccc132c1ef2e15
Jamel Yassin <jamelyassin84@gmail.com>
2021-04-24 00:43:26 +0800

    profile

--- web/src/components/Settings/Profile/Profile.tsx
+++ web/src/components/Settings/Profile/Profile.tsx
@@ -103,6 +103,45 @@ export default function Profile() {
                     </div>
                 </div>
             </div>
+
+
+            <h3 className="mb-0">New Documents</h3>
+            <p className="text-muted mt-0">Previous documents will not be overwritten</p>
+            <div className='card shadow mb-4 mt-1 mb-5'>
+                <div className='card-body'>
+                    <form action='/file-upload' className='dropzone bg-light rounded-lg' id='tinydash-dropzone'>
+                        <div className='dz-message needsclick'>
+                            <div className='circle circle-lg bg-primary'>
+                                <i className='fe fe-upload fe-24 text-white'></i>
+                            </div>
+                            <h5 className='text-muted mt-4'>Drop files here or click to upload</h5>
+                        </div>
+                    </form>
+                    <div className='dropzone-previews mt-3' id='file-previews'></div>
+                    <div className='d-none' id='uploadPreviewTemplate'>
+                        <div className='p-2'>
+                            <div className='row align-items-center'>
+                                <div className='col-auto'>
+                                    <img data-dz-thumbnail src='#' className='avatar-sm rounded bg-light' alt='' />
+                                </div>
+                                <div className='col pl-0'>
+                                    <a
+                                        href='javascript:void(0);'
+                                        className='text-muted font-weight-bold'
+                                        data-dz-name></a>
+                                    <p className='mb-0' data-dz-size></p>
+                                </div>
+                                <div className='col-auto'>
+                                    <a href='' className='btn btn-link btn-lg text-muted' data-dz-remove>
+                                        <i className='dripicons-cross'></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
             <div className="row mb-4">
                 <div className="col-md-6">
                     <div className="form-group">
