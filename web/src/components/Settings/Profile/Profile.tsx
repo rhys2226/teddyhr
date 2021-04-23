ddf397238780d8a80ab07ea95ae765f19b3d679e
Jamel Yassin <jamelyassin84@gmail.com>
2021-04-24 00:26:02 +0800

    profile

---
+++ web/src/components/Settings/Profile/Profile.tsx
@@ -0,0 +1,135 @@
+import React from 'react'
+
+export default function Profile() {
+    return (
+        <div>
+            <div className="row mt-5 align-items-center">
+                <div className="col-md-3 text-center mb-5">
+                    <div className="avatar avatar-xl">
+                        <img style={{ cursor: 'pointer' }} src="./assets/avatars/face-1.jpg" alt="..." className="avatar-img   rounded-circle " />
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="row align-items-center">
+                        <div className="col-md-7">
+                            <h4 className="mb-1">Fuentevilla, Teddy Doe</h4>
+                            <p className="small mb-3"><span className="badge bg-primary-lighter text-primary p-1 br-2" style={{ fontWeight: 900, }}>Developer</span></p>
+                        </div>
+                    </div>
+                    <div className="row mb-4">
+                        <div className="col-md-7">
+                            <p className="text-muted"> Bachelor or Science in Information Technology, Masters in Information Technology, Phd in Information Technology </p>
+                        </div>
+                        <div className="col">
+                            <p className="small mb-0 text-muted">teddy@gmail.com</p>
+                            <p className="small mb-0 text-muted">Vertically Alligned</p>
+                            <p className="small mb-0 text-muted">0928-624-1875</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr />
+            <div className="my-4">
+                <div className="form-row">
+                    <div className="form-group col-md-4">
+                        <label>Firstname</label>
+                        <input type="text" id="firstname" className="form-control" />
+                    </div>
+                    <div className="form-group col-md-3">
+                        <label>Lastname</label>
+                        <input type="text" id="lastname" className="form-control" />
+                    </div>
+                    <div className="form-group col-md-3">
+                        <label>
+                            Middle Name</label>
+                        <input type="text" id="lastname" className="form-control" />
+                    </div>
+                    <div className="form-group col-md-2">
+                        <label> Ext.</label>
+                        <input type="text" id="lastname" className="form-control" />
+                    </div>
+                </div>
+                <div className="form-row">
+                    <div className="form-group col-md-6">
+                        <label >Phone</label>
+                        <input type="email" className="form-control" />
+                    </div>
+                    <div className="form-group col-md-6">
+                        <label >Email</label>
+                        <input type="email" className="form-control" />
+                    </div>
+                </div>
+                <div className="form-group">
+                    <label>Degrees (Separated with comma (,))</label>
+                    <textarea className='form-control' placeholder="Bachelor or Science in Information Technology, Masters in Information Technology, Phd in Information Technology "></textarea>
+                </div>
+                <div className="form-row">
+                    <div className="form-group col-md-12">
+                        <label>Alignment from Bachelors to  Post-graduate </label>
+                        <select className='form-control'>
+                            <option>Vertical</option>
+                            <option>Non-Vertical</option>
+                        </select>
+                    </div>
+                    <div className="form-group col-md-6">
+                        <label>Educational Attainments</label>
+                        <select multiple className='form-control'>
+                            <option>Phd</option>
+                            <option>Masters</option>
+                            <option>Bachelor</option>
+                            <option>Vocational</option>
+                            <option>Secondary</option>
+                            <option>Primary</option>
+                        </select>
+                    </div>
+                    <div className="form-group col-md-6">
+                        <label>Eligibilities</label>
+                        <select multiple className='form-control'>
+                            <option>Civil Service</option>
+                            <option>PRC</option>
+                        </select>
+                    </div>
+                    <div className='form-group mb-3 col-12 mt-2'>
+                        <label>Work Experiences</label>
+                        <textarea className='form-control'></textarea>
+                    </div>
+                    <div className='form-group mb-3 col-12 col-md-6'>
+                        <label>Previous Employer</label>
+                        <input type='text' className='form-control' />
+                    </div>
+                    <div className='form-group mb-3 col-12 col-md-6'>
+                        <label>Employer's Contact Information</label>
+                        <input type='text' className='form-control' />
+                    </div>
+                </div>
+            </div>
+            <div className="row mb-4">
+                <div className="col-md-6">
+                    <div className="form-group">
+                        <label>Old Password</label>
+                        <input type="password" className="form-control" id="inputPassword5" />
+                    </div>
+                    <div className="form-group">
+                        <label>New Password</label>
+                        <input type="password" className="form-control" id="inputPassword5" />
+                    </div>
+                    <div className="form-group">
+                        <label>Confirm Password</label>
+                        <input type="password" className="form-control" id="inputPassword6" />
+                    </div>
+                </div>
+                <div className="col-md-6">
+                    <p className="mb-2">Password requirements</p>
+                    <p className="small text-muted mb-2"> To create a new password, you have to meet all of the following requirements: </p>
+                    <ul className="small text-muted pl-4 mb-0">
+                        <li> Minimum 8 character </li>
+                        <li>At least one special character</li>
+                        <li>At least one number</li>
+                        <li>Can’t be the same as a previous password </li>
+                    </ul>
+                </div>
+            </div>
+            <button type="submit" className="btn btn-danger">Save Changes</button>
+        </div>
+    )
+}
--- web/src/views/Settings.tsx
+++ web/src/views/Settings.tsx
@@ -1,25 +1,33 @@
 import React from 'react'
+import Profile from '../components/Settings/Profile/Profile'
 
 export default function Settings() {
     return (
-        <div>
-            <ul className="nav nav-tabs mb-4" id="myTab" role="tablist">
-                <li className="nav-item">
-                    <a className="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Profile</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Personal Data Sheet</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Leave Card</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">ICPR</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Service Record</a>
-                </li>
-            </ul>
+        <div className="row justify-content-center">
+            <div className="col-12 col-lg-10 col-xl-8">
+                <h2 className="h3 mb-4 page-title">Settings</h2>
+                <div className="my-4">
+                    <ul className="nav nav-tabs mb-4" id="myTab" role="tablist">
+                        <li className="nav-item">
+                            <a className="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Profile</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Personal Data Sheet</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Leave Card</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">ICPR</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Service Record</a>
+                        </li>
+                    </ul>
+
+                    <Profile />
+                </div>
+            </div>
         </div>
     )
 }
