e7f1f35f2cf26c5cdbe5065c137616371754c08b
Jamel Yassin <jamelyassin84@gmail.com>
2021-04-23 22:55:50 +0800

    changes

--- web/src/components/main/Content.tsx
+++ web/src/components/main/Content.tsx
@@ -3,8 +3,14 @@ import React from 'react'
 export default function Content( props: any ) {
     return (
         <div>
-            content ni
-            {props.children}
+            <main role="main" className="main-content">
+                <div className="container-fluid">
+                    <div className="row justify-content-center">
+                        <div className="col-12">
+                            {props.children}
+                        </div></div>
+                </div>
+            </main>
         </div>
     )
 }
