a4d82eed7c7eb0116837a19f16a5117951ea0e76
Jamel Yassin <jamelyassin84@gmail.com>
2021-04-24 14:03:34 +0800

    add-employee-form

--- web/src/components/main/Content.tsx
+++ web/src/components/main/Content.tsx
@@ -4,6 +4,7 @@ import Settings from '../../views/Settings/Settings'
 import Overview from '../../views/Settings/Overview/Overview'
 import Employees from '../../views/Users/Employees/Employees'
 import AddVancancy from '../../views/Recruitement/Add-vacancy/AddVancancy'
+import AddEmployee from '../../views/Users/Add-Employee/AddEmployee'
 
 export default function Content( props: any ) {
     return (
@@ -16,6 +17,7 @@ export default function Content( props: any ) {
 
                             {/* <AddVancancy /> */}
                             {/* <Settings /> */}
+                            <AddEmployee />
                             {/* <Applicants /> */}
                             {/* <Employees /> */}
 
