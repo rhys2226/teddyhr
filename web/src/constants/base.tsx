d703186d89e16267a7d9831f3486269e1032c6fc
Jamel Yassin <jamelyassin84@gmail.com>
2021-04-25 20:22:22 +0800

    front-end

--- web/src/constants/base.tsx
+++ web/src/constants/base.tsx
@@ -1,3 +1,5 @@
 
 
 export const route = "http://localhost:3000"
+export const api = 'http://127.0.0.1:8000/api/'
+
--- web/src/views/Main/Login.tsx
+++ web/src/views/Main/Login.tsx
@@ -1,10 +1,35 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { useHistory } from "react-router-dom";
+import axios from 'axios';
+import * as base from './../../constants/base'
+import { Alert, Fire } from '../../components/Alerts/Alert';
 
 type Props = {};
 
 const Login: FC<Props> = ( props ) => {
     const history = useHistory()
+
+    const [ username, setusername ] = useState( '' )
+    const [ password, setpassword ] = useState( '' )
+    const [ disabled, setdisabled ] = useState( false )
+
+    async function login() {
+        setdisabled( true )
+        const data = {
+            username: username,
+            password: password,
+        }
+        const url = `${ base.api }auth/login`
+        await axios.post( url, data )
+            .then( function ( response ) {
+                $( '.modal-backdrop' ).toggle();
+                history.replace( 'home' )
+            } ).catch( () => [
+                Alert( 'Error', 'Invalid Username or Password', 'error' )
+            ] )
+        setdisabled( false )
+
+    }
 
     return (
         <div className='row align-items-center h-100'>
@@ -19,7 +44,7 @@ const Login: FC<Props> = ( props ) => {
                             <i className=" fe fe-user"></i>
                         </span>
                     </div>
-                    <input autoFocus type="text" className="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" />
+                    <input onChange={( e ) => { setusername( e.target.value ) }} type="text" className="form-control" placeholder="Username" />
                 </div>
                 <div className="input-group mb-3">
                     <div className="input-group-prepend">
@@ -27,17 +52,14 @@ const Login: FC<Props> = ( props ) => {
                             <i className=" fe fe-lock"></i>
                         </span>
                     </div>
-                    <input type="password" className="form-control" placeholder="Password" aria-label="Username" aria-describedby="basic-addon1" />
+                    <input onChange={( e ) => { setpassword( e.target.value ) }} type="password" className="form-control" placeholder="Password" />
                 </div>
-                <button onClick={() => {
-                    $( '.modal-backdrop' ).toggle();
-                    history.replace( 'home' )
-                }} className='btn btn-lg btn-danger btn-block' type='submit'>
+                <button disabled={disabled} onClick={( e ) => { e.preventDefault(); login() }} className='btn btn-lg btn-danger btn-block' type='submit'>
                     Login
                 </button>
                 <p className='mt-5 mb-3 text-muted'>© Developed By | Teddy Fuentivilla </p>
             </form>
-        </div>
+        </div >
     );
 };
 
