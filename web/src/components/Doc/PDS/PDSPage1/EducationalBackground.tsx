ebc95e006718dc68158fb5cd4e7796aeef9ce4b0
Jamel Yassin <jamelyassin84@gmail.com>
2021-05-06 09:11:58 +0800

    borders fixed

--- web/src/components/Doc/PDS/PDSPage1/EducationalBackground.tsx
+++ web/src/components/Doc/PDS/PDSPage1/EducationalBackground.tsx
@@ -6,145 +6,146 @@ export default function EducationalBackground() {
             <div className="title "><i className="text-white main">III. Educational Background</i></div>
             
              <div className="box row sub-title m-0 p-0 m-0 p-0">
-                <div className="col-md-2 m-0 p-0 row sub-title m-0 p-0 d-flex aic jcc">
+                <div className="col-md-2 m-0 p-0 row sub-title m-0 p-0 d-flex aic jcc educ-level nbl nbr nbb">
                     <p className="m-0 mb-0 pb-0 text-center">&nbsp; <span style={{position:'absolute', left:'.3rem',top:'.1rem'}}>2.</span>  LEVEL</p>
                 </div>
-                <div className="col-md-2 m-0 p-0 row sub-title m-0 p-0 d-flex aic jcc">
+                <div className="col-md-2 m-0 p-0 row sub-title m-0 p-0 d-flex aic jcc educ-level nbr nbb">
                     <p className="m-0 mb-0 pb-0 text-center fs-sm">&nbsp;NAME OF SCHOOL (Write in full)</p>
                 </div>
-                <div className="col-md-2 row sub-title m-0 p-0 d-flex aic jcc">
-                    <p className="m-0 mb-0 pb-0 text-center fs-sm">&nbsp;BASIC EDUCATION/DEGREE/COURSE                     </p>
+                <div className="col-md-2 row sub-title m-0 p-0 d-flex aic jcc educ-level  nbb">
+                    <p className="m-0 mb-0 pb-0 text-center fs-sm">&nbsp;BASIC EDUCATION/DEGREE/COURSE</p>
                 </div>
-                <div className="col-md-2  row sub-title m-0 p-0 d-flex aic jcc">
+                <div className="col-md-1  row sub-title m-0 p-0 d-flex aic jcc from-to  nbb">
                     <div className="row p-0 m-0">
-                        <p className="m-0 mb-0 text-center fs-sm col-md-12 border-bottom mt-3 nbt">&nbsp;PERIOD OF ATTENDANCE</p>
+                        <p className="m-0 mb-0 text-center fs-sm col-md-12 border-bottom mt-3 nbt nbb" >&nbsp;PERIOD OF ATTENDANCE</p>
                         <p className="m-0 mb-0 text-center fs-sm col-md-6 box nbl nbr nbb nbt">&nbsp;From</p>
                         <p className="m-0 mb-0 text-center fs-sm col-md-6 box nbr nbb nbt">&nbsp;To</p>
                     </div>
                 </div>
-                <div className="col-md-2 row sub-title m-0 p-0 d-flex aic jcc">
+                <div className="col-md-1 row sub-title m-0 p-0 d-flex aic jcc nbr nbb">
                     <p className="m-0 mb-0 text-center fs-sm">&nbsp;HIGHEST LEVEL/ UNITS EARNED (if not graduated)</p>
                 </div>
-                <div className="col-md-1 row sub-title m-0 p-0 d-flex aic jcc">
+                <div className="col-md-1 row sub-title m-0 p-0 d-flex aic jcc nbr nbb">
                     <p className="m-0 mb-0 text-center fs-sm">&nbsp;YEAR GRADUATED </p>
                 </div>
-                <div className="col-md-1  row sub-title m-0 p-0 d-flex aic jcc">
+                <div className="col-md-1  row sub-title m-0 p-0 d-flex aic jcc nbr nbb ">
                     <p className="m-0 mb-0 pb-0 text-center fs-sm">&nbsp;SCHOLARSHIP/ ACADEMIC HONORS RECEIVED</p>
                 </div>
                 
                 
-                <div className="col-md-2 m-0 p-0 row sub-title m-0 p-0">
+                <div className="col-md-2 m-0 p-0 row sub-title m-0 p-0 educ-level nbl nbr nbb ">
                     <p className="m-0 mb-0 p-1 text-center">&nbsp;   ELEMENTARY</p>
                 </div>
-                <div className="col-md-2 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc">
+                <div className="col-md-2 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc educ-level  nbr nbb">
                     <p className="m-0 mb-0 p-1 text-center bold underlined">N/A</p>
                 </div>
-                <div className="col-md-2 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc">
+                <div className="col-md-1 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc educ-level   nbb">
                     <p className="m-0 mb-0 p-1 text-center bold underlined">N/A</p>
                 </div>
-                <div className="col-md-2  row m-0 p-0 d-flex aic jcc bg-white box">
+                <div className="col-md-1  row m-0 p-0 d-flex aic jcc bg-white box from-to   nbb">
                     <p className="m-0 mb-0 text-center fs-sm col-md-6 box nbl nbr nbb nbt bold underlined">&nbsp;N/A</p>
                     <p className="m-0 mb-0 text-center fs-sm col-md-6 box nbr nbb nbt bold underlined">&nbsp;N/A</p>
                 </div>
-                <div className="col-md-2 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc">
+                <div className="col-md-1 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc  nbr nbb">
                     <p className="m-0 mb-0 p-1 text-center bold underlined">N/A</p>
                 </div>
-                <div className="col-md-1 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc">
+                <div className="col-md-1 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc  nbr nbb">
                     <p className="m-0 mb-0 p-1 text-center bold underlined">N/A</p>
                 </div>
-                <div className="col-md-1 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc">
+                <div className="col-md-1 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc  nbr nbb">
                     <p className="m-0 mb-0 p-1 text-center bold underlined">N/A</p>
                 </div>
+             
                 
-                <div className="col-md-2 m-0 p-0 row sub-title m-0 p-0">
+                <div className="col-md-2 m-0 p-0 row sub-title m-0 p-0 educ-level nbr nbb nbl nbr">
                     <p className="m-0 mb-0 p-1 text-center">&nbsp;   SECONDARY</p>
                 </div>
-                <div className="col-md-2 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc">
+                <div className="col-md-2 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc educ-level nbr nbb">
                     <p className="m-0 mb-0 p-1 text-center bold underlined">N/A</p>
                 </div>
-                <div className="col-md-2 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc">
+                <div className="col-md-2 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc educ-level  nbb">
                     <p className="m-0 mb-0 p-1 text-center bold underlined">N/A</p>
                 </div>
-              <div className="col-md-2  row m-0 p-0 d-flex aic jcc bg-white box">
+              <div className="col-md-1  row m-0 p-0 d-flex aic jcc bg-white box from-to  nbb">
                     <p className="m-0 mb-0 text-center fs-sm col-md-6 box nbl nbr nbb nbt bold underlined">&nbsp;N/A</p>
                     <p className="m-0 mb-0 text-center fs-sm col-md-6 box nbr nbb nbt bold underlined">&nbsp;N/A</p>
                 </div>
-                <div className="col-md-2 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc">
+                <div className="col-md-1 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc nbr nbb">
                     <p className="m-0 mb-0 p-1 text-center bold underlined">N/A</p>
                 </div>
-                <div className="col-md-1 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc">
+                <div className="col-md-1 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc nbr nbb">
                     <p className="m-0 mb-0 p-1 text-center bold underlined">N/A</p>
                 </div>
-                <div className="col-md-1 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc">
+                <div className="col-md-1 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc nbr nbb">
                     <p className="m-0 mb-0 p-1 text-center bold underlined">N/A</p>
                 </div>
                 
-                <div className="col-md-2 m-0 p-0 row sub-title m-0 p-0 ">
+                <div className="col-md-2 m-0 p-0 row sub-title m-0 p-0 educ-level nbr nbb nbl">
                     <p className="m-0 mb-0 p-1 text-center">VOCATIONAL/ <br/>&nbsp;  TRADE COURSE</p>
                 </div>
-                <div className="col-md-2 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc">
+                <div className="col-md-2 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc educ-level nbr nbb">
                     <p className="m-0 mb-0 p-1 text-center bold underlined">N/A</p>
                 </div>
-                <div className="col-md-2 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc">
+                <div className="col-md-2 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc educ-level  nbb">
                     <p className="m-0 mb-0 p-1 text-center bold underlined">N/A</p>
                 </div>
-                 <div className="col-md-2  row m-0 p-0 d-flex aic jcc bg-white box">
+                 <div className="col-md-1  row m-0 p-0 d-flex aic jcc bg-white box from-to  nbb">
                     <p className="m-0 mb-0 text-center fs-sm col-md-6 box nbl nbr nbb nbt bold underlined">&nbsp;N/A</p>
                     <p className="m-0 mb-0 text-center fs-sm col-md-6 box nbr nbb nbt bold underlined">&nbsp;N/A</p>
                 </div>
-                <div className="col-md-2 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc">
+                <div className="col-md-1 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc nbr nbb">
                     <p className="m-0 mb-0 p-1 text-center bold underlined">N/A</p>
                 </div>
-                <div className="col-md-1 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc">
+                <div className="col-md-1 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc nbr nbb">
                     <p className="m-0 mb-0 p-1 text-center bold underlined">N/A</p>
                 </div>
-                <div className="col-md-1 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc">
+                <div className="col-md-1 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc nbr nbb">
                     <p className="m-0 mb-0 p-1 text-center bold underlined">N/A</p>
                 </div>
                 
-                <div className="col-md-2 m-0 p-0 row sub-title m-0 p-0">
+                <div className="col-md-2 m-0 p-0 row sub-title m-0 p-0 educ-level nbr nbb nbl">
                     <p className="m-0 mb-0 p-1 text-center">&nbsp;   COLLEGE</p>
                 </div>
-                <div className="col-md-2 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc">
+                <div className="col-md-2 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc educ-level nbr nbb">
                     <p className="m-0 mb-0 p-1 text-center bold underlined">N/A</p>
                 </div>
-                <div className="col-md-2 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc">
+                <div className="col-md-2 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc educ-level  nbb">
                     <p className="m-0 mb-0 p-1 text-center bold underlined">N/A</p>
                 </div>
-                  <div className="col-md-2  row m-0 p-0 d-flex aic jcc bg-white box">
+                  <div className="col-md-2  row m-0 p-0 d-flex aic jcc bg-white box from-to  nbb">
                     <p className="m-0 mb-0 text-center fs-sm col-md-6 box nbl nbr nbb nbt bold underlined">&nbsp;N/A</p>
                     <p className="m-0 mb-0 text-center fs-sm col-md-6 box nbr nbb nbt bold underlined">&nbsp;N/A</p>
                 </div>
-                <div className="col-md-2 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc">
+                <div className="col-md-1 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc nbr nbb">
                     <p className="m-0 mb-0 p-1 text-center bold underlined">N/A</p>
                 </div>
-                <div className="col-md-1 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc">
+                <div className="col-md-1 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc nbr nbb">
                     <p className="m-0 mb-0 p-1 text-center bold underlined">N/A</p>
                 </div>
-                <div className="col-md-1 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc">
+                <div className="col-md-1 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc nbr nbb">
                     <p className="m-0 mb-0 p-1 text-center bold underlined">N/A</p>
                 </div>
                 
-                <div className="col-md-2 m-0 p-0 row sub-title m-0 p-0">
+                <div className="col-md-2 m-0 p-0 row sub-title m-0 p-0 educ-level nbr nbb nbl">
                     <p className="m-0 mb-0 p-1 text-center">&nbsp; GRADUATE STUDIES </p>
                 </div>
-                <div className="col-md-2 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc">
+                <div className="col-md-2 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc educ-level nbr     ">
                     <p className="m-0 mb-0 p-1 text-center bold underlined">N/A</p>
                 </div>
-                <div className="col-md-2 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc">
+                <div className="col-md-2 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc educ-level      ">
                     <p className="m-0 mb-0 p-1 text-center bold underlined">N/A</p>
                 </div>
-                <div className="col-md-2  row m-0 p-0 d-flex aic jcc bg-white box">
+                <div className="col-md-1  row m-0 p-0 d-flex aic jcc bg-white box from-to    ">
                     <p className="m-0 mb-0 text-center fs-sm col-md-6 box nbl nbr nbb nbt bold underlined">&nbsp;N/A</p>
                     <p className="m-0 mb-0 text-center fs-sm col-md-6 box nbr nbb nbt bold underlined">&nbsp;N/A</p>
                 </div>
-                <div className="col-md-2 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc">
+                <div className="col-md-1 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc nbr    ">
                     <p className="m-0 mb-0 p-1 text-center bold underlined">N/A</p>
                 </div>
-                <div className="col-md-1 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc">
+                <div className="col-md-1 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc nbr    ">
                     <p className="m-0 mb-0 p-1 text-center bold underlined">N/A</p>
                 </div>
-                <div className="col-md-1 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc">
+                <div className="col-md-1 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc nbr    ">
                     <p className="m-0 mb-0 p-1 text-center bold underlined">N/A</p>
                 </div>
                 
@@ -154,20 +155,20 @@ export default function EducationalBackground() {
                     </p>
                 </div>
                 
-                {/* <div className="row col-md-12">
-                    <div className="col-md-2 m-0 p-0 row m-0 p-0 box  d-flex aic jcc">
+                <div className="row col-md-12 ">
+                    <div className="col-md-2 m-0 p-0 row m-0 p-0 box  d-flex aic jcc  sig nbr nbb nbl">
                         <p className="m-0 mb-0 p-1 text-center bold"><i>SIGNATURE</i></p>
                     </div>
-                    <div className="col-md-4 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc">
+                    <div className="col-md-4 m-0 p-0 row m-0 p-0 box bg-white d-flex aic jcc sig1  nbb">
                         <p className="m-0 mb-0 p-1 text-center bold underlined">N/A</p>
                     </div>
-                    <div className="col-md-2 m-0 p-0 row m-0 p-0 box  d-flex aic jcc">
+                    <div className="col-md-2 m-0 p-0 row m-0 p-0 box  d-flex aic jcc sig2  nbb bl">
                         <p className="m-0 mb-0 p-1 text-center bold"><i>DATE</i></p>
                     </div>
-                    <div className="col-md-4 m-0 p-0 row m-0 p-0 box  d-flex aic jcc">
+                    <div className="col-md-4 m-0 p-0 row m-0 p-0 box  d-flex aic jcc  bg-white sig3 nbr nbb">
                         <p className="m-0 mb-0 p-1 text-center bold underlined">N/A</p>
                     </div>
-                </div> */}
+                </div>
             </div>
         </div>
     )
