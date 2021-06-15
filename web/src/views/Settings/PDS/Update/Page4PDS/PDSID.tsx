ee493b3c7f7a4c5b3ab1c047ad0128e961bf55a5
Jamel Yassin <jamelyassin84@gmail.com>
2021-06-15 14:18:39 +0800

    identification

--- web/src/views/Settings/PDS/Update/Page4PDS/PDSID.tsx
+++ web/src/views/Settings/PDS/Update/Page4PDS/PDSID.tsx
@@ -1,38 +1,75 @@
 import React from 'react'
-import { Fire } from '../../../../../components/Alerts/Alert'
+import { Alert, Fire } from '../../../../../components/Alerts/Alert'
+import { useForm } from 'react-hook-form'
+import { Auth } from '../../../../../services/auth.service'
+
+type Inputs = {
+    employee_id: undefined,
+    GovtID: undefined,
+    Passport: undefined,
+    Date: undefined,
+}
+
 
 export default function PDSID( props: any ) {
+
+    const userData: any = localStorage.getItem( 'user' )
+    const { register, handleSubmit } = useForm<Inputs>();
+
+    const submit = async ( data: any ) => {
+        Fire(
+            'Update Personal Data Sheet?',
+            'I declare under oath that I have personally accomplished this Personal Data Sheet which is a true, correct and complete statement pursuant to the provisions of pertinent laws, rules and regulations of the Republic of the Philippines. I authorize the agency head/authorized representative to verify/validate the contents stated herein.I agree that any misrepresentation made in this document and its attachments shall cause the filing of administrative/criminal case/s against me.',
+            'info',
+            () => {
+                console.log( data )
+                let api = new Auth( 'identification' )
+                api.create( data, {} ).then( () => {
+                    Alert(
+                        'PDS Successfully Updated',
+                        'You have successfully updated your Personal Data Sheet',
+                        'success',
+                    )
+                } )
+                    .catch( () => {
+                        Alert(
+                            'Error',
+                            'Erro Updating Personal Data Sheet. Try Again',
+                            'error',
+                        )
+                    } )
+            }
+        )
+    }
+
+
     return (
         <div className="card-body card">
-            <h5 className="bold mt-4 mb-4">IDENTIFICATION</h5>
+            <form onSubmit={handleSubmit( submit )}>
+                <h5 className="bold mt-4 mb-4">IDENTIFICATION</h5>
 
-            <h6 className="bold mt-4 mb-4 text-info">Government Issued ID (i.e.Passport, GSIS, SSS, PRC, Driver's License, etc)PLEASE INDICATE ID Number and Date of Issuance</h6>
+                <h6 className="bold mt-4 mb-4 text-info">Government Issued ID (i.e.Passport, GSIS, SSS, PRC, Driver's License, etc)PLEASE INDICATE ID Number and Date of Issuance</h6>
 
-            <div className="row mb-4">
-                <div className="col-md-4 mb-4">
-                    <label htmlFor="">Government Issued ID</label>
-                    <input type="text" className="form-control" />
+                <div className="row mb-4">
+                    <input type="hidden" value={JSON.parse( userData ).id}  {...register( 'employee_id' )} />
+                    <div className="col-md-4 mb-4">
+                        <label htmlFor="">Government Issued ID</label>
+                        <input  {...register( 'GovtID' )} type="text" className="form-control" />
+                    </div>
+                    <div className="col-md-4 mb-4">
+                        <label htmlFor="">ID/License/Passport No</label>
+                        <input  {...register( 'Passport' )} type="text" className="form-control" />
+                    </div>
+                    <div className="col-md-4 mb-4">
+                        <label htmlFor="">Date/Place of IssuancE</label>
+                        <input  {...register( 'Date' )} type="text" className="form-control" />
+                    </div>
                 </div>
-                <div className="col-md-4 mb-4">
-                    <label htmlFor="">ID/License/Passport No</label>
-                    <input type="text" className="form-control" />
-                </div>
-                <div className="col-md-4 mb-4">
-                    <label htmlFor="">Date/Place of IssuancE</label>
-                    <input type="text" className="form-control" />
-                </div>
-            </div>
 
-            <div className="mt-2 mb-4 d-flex aij jcc">
-                <button onClick={() => {
-                    Fire(
-                        'Update Personal Data Sheet?',
-                        ' I declare under oath that I have personally accomplished this Personal Data Sheet which is a true, correct and complete statement pursuant to the provisions of pertinent laws, rules and regulations of the Republic of the Philippines. I authorize the agency head/authorized representative to verify/validate the contents stated herein.I agree that any misrepresentation made in this document and its attachments shall cause the filing of administrative/criminal case/s against me.',
-                        'info',
-                        () => { }
-                    )
-                }} className="btn btn-outline-primary">Update Identification</button>
-            </div>
+                <div className="mt-2 mb-4 d-flex aij jcc">
+                    <button className="btn btn-outline-primary">Update identification</button>
+                </div>
+            </form>
         </div>
     )
 }
