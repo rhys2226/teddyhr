72695bb7a86cfd3233936b18109456709503661d
Jamel Yassin <jamelyassin84@gmail.com>
2021-06-15 14:13:30 +0800

    references

--- server/app/Http/Controllers/ReferencesController.php
+++ server/app/Http/Controllers/ReferencesController.php
@@ -8,33 +8,29 @@ use Illuminate\Http\Request;
 
 class ReferencesController extends Controller
 {
-    public function index()
-    {
-        //
-    }
-
     public function store(Request $request)
     {
-        //
+        $data = $request->all();
+        $pds = References::where('employee_id',$data['employee_id'])->get();
+        if(count($pds) !== 0){
+            foreach($pds as $sheet){
+                References::find($sheet->id)->delete();
+            }
+        }
+        $index = 0;
+            foreach($data['references'] as $child){
+            $experience = new References(); 
+            $experience->employee_id= $data['employee_id'];
+            $experience->Name= $child['Name'.$index];
+            $experience->Address= $child['Address'.$index];
+            $experience->TelNumber= $child['TelNumber'.$index];
+            $experience->save();
+            $index+=1;
+        }
     }
 
-    public function show(References $references)
+    public function show($id)
     {
-        //
-    }
-
-    public function edit(References $references)
-    {
-        //
-    }
-
-    public function update(Request $request, References $references)
-    {
-        //
-    }
-
-    public function destroy(References $references)
-    {
-        //
+    return References::where('employee_id',$id)->get();
     }
 }
