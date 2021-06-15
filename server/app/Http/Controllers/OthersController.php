5fd9077d26f0a08b77b417be20bb4ee5d6460434
Jamel Yassin <jamelyassin84@gmail.com>
2021-06-15 14:07:46 +0800

    others

--- server/app/Http/Controllers/OthersController.php
+++ server/app/Http/Controllers/OthersController.php
@@ -8,28 +8,29 @@ use Illuminate\Http\Request;
 
 class OthersController extends Controller
 {
-    public function index()
-    {
-        //
-    }
-
     public function store(Request $request)
     {
-        //
+        $data = $request->all();
+        $pds = Others::where('employee_id',$data['employee_id'])->get();
+        if(count($pds) !== 0){
+            foreach($pds as $sheet){
+                Others::find($sheet->id)->delete();
+            }
+        }
+        $index = 0;
+        foreach($data['others'] as $other){
+            $experience = new Others(); 
+            $experience->employee_id= $data['employee_id'];
+            $experience->Skills= $other['Skills'.$index];
+            $experience->NonAcademicDistinction= $other['NonAcademicDistinction'.$index];
+            $experience->Membership= $other['Membership'.$index];
+            $experience->save();
+            $index+=1;
+        }
     }
 
-    public function show(Others $others)
+    public function show($id)
     {
-        //
-    }
-
-    public function update(Request $request, Others $others)
-    {
-        //
-    }
-
-    public function destroy(Others $others)
-    {
-        //
+        return Others::where('employee_id',$id)->get();
     }
 }
