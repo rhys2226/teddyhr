5f84387dbc3005172a3157f878c3e75f232d0999
Jamel Yassin <jamelyassin84@gmail.com>
2021-06-15 13:34:43 +0800

    eligibilities and work experience

--- web/src/views/Settings/PDS/Update/Page2PDS/PDSEligibility.tsx
+++ web/src/views/Settings/PDS/Update/Page2PDS/PDSEligibility.tsx
@@ -1,56 +1,119 @@
 import React from 'react'
-import { Fire } from '../../../../../components/Alerts/Alert'
+import { Alert, Fire } from '../../../../../components/Alerts/Alert'
+import { Auth } from '../../../../../services/auth.service'
+
 
 export default function PDSEligibility( props: any ) {
+
+    const userData: any = localStorage.getItem( 'user' )
+    const [ addForm, setaddForm ] = React.useState( [ 1 ] );
+
+    const submit = async () => {
+        let data: any = {
+            eligibilities: []
+        }
+        for ( let i = 0; i < addForm.length; i++ ) {
+            let input = $( '.data' + i + ' input' )
+            let object: any = {}
+            for ( let index in input ) {
+                if ( input[ index ].id == undefined ) {
+                    break
+                }
+                object[ input[ index ].id ] = $( '#' + input[ index ][ 'id' ] ).val()
+            }
+            data[ 'eligibilities' ].push( object )
+        }
+        data[ 'employee_id' ] = JSON.parse( userData ).id
+
+        Fire(
+            'Update Personal Data Sheet?',
+            'I declare under oath that I have personally accomplished this Personal Data Sheet which is a true, correct and complete statement pursuant to the provisions of pertinent laws, rules and regulations of the Republic of the Philippines. I authorize the agency head/authorized representative to verify/validate the contents stated herein.I agree that any misrepresentation made in this document and its attachments shall cause the filing of administrative/criminal case/s against me.',
+            'info',
+            () => {
+                let api = new Auth( 'eligibilities' )
+                api.create( data, {} ).then( () => {
+                    Alert(
+                        'PDS Successfully Updated',
+                        'You have successfully updated your Personal Data Sheet',
+                        'success',
+                    )
+                } )
+                    .catch( () => {
+                        Alert(
+                            'Error',
+                            'Erro Updating Personal Data Sheet. Try Again',
+                            'error',
+                        )
+                    } )
+            }
+        )
+    }
+
+
     return (
         <div className="card-body card">
-            <h5 className="bold mt-4 mb-4">CIVIL SERVICE ELIGIBILITY</h5>
-            <h6 className="bold mt-4 mb-4 text-info">CAREER SERVICE/ RA 1080 (BOARD/ BAR) UNDER
-            SPECIAL LAWS/ CES/ CSEEBARANGAY ELIGIBILITY / DRIVER'S LICENSE</h6>
-
-            <div className="col-md-6 mb-4 p-0 m-0">
-                <button className="btn btn-outline-dark d-flex aic jcc">
+            <h5 className="bold mt-4 m-0 ">CIVIL SERVICE ELIGIBILITY
+                <br />
+                <small className="text-muted" style={{ fontSize: '.8rem' }}> CAREER SERVICE/ RA 1080 (BOARD/ BAR) UNDER
+                    SPECIAL LAWS/ CES/ CSEEBARANGAY ELIGIBILITY / DRIVER'S LICENSE</small>
+            </h5>
+            <br />
+            <br />
+            <div className="col-md-6 mb-4 p-0 m-0 d-flex">
+                <button
+                    onClick={() => {
+                        setaddForm( [ ...addForm, 1 ] );
+                    }} className="btn btn-outline-dark d-flex aic jcc">
                     <i className="fe fe-plus"></i>
                     <span> Add Eligibility</span>
                 </button>
+                <button
+                    type="button"
+                    onClick={() => {
+                        setaddForm( [ 1 ] );
+                    }}
+                    className="btn btn-outline-danger d-flex aic jcc ml-3">
+                    <i className="fe fe-plus"></i>
+                    <span> Reset</span>
+                </button>
             </div>
 
-            <h6 className="bold mt-4 mb-4 text-info">ELIGIBILITY 1</h6>
-            <div className="row mb-4">
-                <div className="col-md-12 mb-4">
-                    <label htmlFor="">ELIGIBILITY TITILE</label>
-                    <input type="text" className="form-control" />
-                </div>
-                <div className="col-md-6 mb-4">
-                    <label htmlFor="">RATING(If Applicable)</label>
-                    <input type="text" className="form-control" />
-                </div>
-                <div className="col-md-6 mb-4">
-                    <label htmlFor="">DATE OF EXAMINATION / CONFERMENT</label>
-                    <input type="text" className="form-control" />
-                </div>  <div className="col-md-12 mb-4">
-                    <label htmlFor=""> PLACE OF EXAMINATION</label>
-                    <input type="text" className="form-control" />
-                </div>
-                <div className="col-md-6 mb-4">
-                    <label htmlFor="">LICENSE NUMBER</label>
-                    <input type="text" className="form-control" />
-                </div>
-                <div className="col-md-6 mb-4">
-                    <label htmlFor="">DATE OF VALIDITY</label>
-                    <input type="date" className="form-control" />
-                </div>
-            </div>
-
+            {
+                addForm.map( ( value, index ) => (
+                    <div key={index} className={`data${ index }`}>
+                        <h6 className={`bold mt-4 mb-4 text-info`}>ELIGIBILITY {index + 1}</h6>
+                        <div className="row mb-4">
+                            <div className="col-md-12 mb-4">
+                                <label htmlFor="">ELIGIBILITY TITILE</label>
+                                <input id={`Title${ index }`} type="text" className="form-control" />
+                            </div>
+                            <div className="col-md-6 mb-4">
+                                <label htmlFor="">RATING(If Applicable)</label>
+                                <input id={`Rating${ index }`} type="text" className="form-control" />
+                            </div>
+                            <div className="col-md-6 mb-4">
+                                <label htmlFor="">DATE OF EXAMINATION / CONFERMENT</label>
+                                <input id={`Date${ index }`} type="text" className="form-control" />
+                            </div>  <div className="col-md-12 mb-4">
+                                <label htmlFor=""> PLACE OF EXAMINATION</label>
+                                <input id={`Place${ index }`} type="text" className="form-control" />
+                            </div>
+                            <div className="col-md-6 mb-4">
+                                <label htmlFor="">LICENSE NUMBER</label>
+                                <input id={`License${ index }`} type="text" className="form-control" />
+                            </div>
+                            <div className="col-md-6 mb-4">
+                                <label htmlFor="">DATE OF VALIDITY</label>
+                                <input id={`Validity${ index }`} type="date" className="form-control" />
+                            </div>
+                        </div>
+                    </div>
+                ) )
+            }
             <div className="mt-2 mb-4 d-flex aij jcc">
-                <button onClick={() => {
-                    Fire(
-                        'Update Personal Data Sheet?',
-                        ' I declare under oath that I have personally accomplished this Personal Data Sheet which is a true, correct and complete statement pursuant to the provisions of pertinent laws, rules and regulations of the Republic of the Philippines. I authorize the agency head/authorized representative to verify/validate the contents stated herein.I agree that any misrepresentation made in this document and its attachments shall cause the filing of administrative/criminal case/s against me.',
-                        'info',
-                        () => { }
-                    )
-                }} className="btn btn-outline-primary">Update Eligibility</button>
+                <button onClick={() => [
+                    submit()
+                ]} className="btn btn-outline-primary">Update Eligibility</button>
             </div>
         </div>
     )
--- web/src/views/Settings/PDS/Update/Page2PDS/PDSWorkExperience.tsx
+++ web/src/views/Settings/PDS/Update/Page2PDS/PDSWorkExperience.tsx
@@ -1,59 +1,126 @@
 import React from 'react'
-import { Fire } from '../../../../../components/Alerts/Alert'
+import { Alert, Fire } from '../../../../../components/Alerts/Alert'
+import { Auth } from '../../../../../services/auth.service'
+
 
 export default function PDSWorkExperience() {
+
+    const userData: any = localStorage.getItem( 'user' )
+    const [ addForm, setaddForm ] = React.useState( [ 1 ] );
+
+    const submit = async () => {
+        let data: any = {
+            workExperiences: []
+        }
+        for ( let i = 0; i < addForm.length; i++ ) {
+            let input = $( '.data' + i + ' input, select ' )
+            let object: any = {}
+            for ( let index in input ) {
+                if ( input[ index ].id == undefined ) {
+                    break
+                }
+                object[ input[ index ].id ] = $( '#' + input[ index ][ 'id' ] ).val()
+            }
+            data[ 'workExperiences' ].push( object )
+        }
+        data[ 'employee_id' ] = JSON.parse( userData ).id
+
+        Fire(
+            'Update Personal Data Sheet?',
+            'I declare under oath that I have personally accomplished this Personal Data Sheet which is a true, correct and complete statement pursuant to the provisions of pertinent laws, rules and regulations of the Republic of the Philippines. I authorize the agency head/authorized representative to verify/validate the contents stated herein.I agree that any misrepresentation made in this document and its attachments shall cause the filing of administrative/criminal case/s against me.',
+            'info',
+            () => {
+                let api = new Auth( 'work-experience' )
+                api.create( data, {} ).then( () => {
+                    Alert(
+                        'PDS Successfully Updated',
+                        'You have successfully updated your Personal Data Sheet',
+                        'success',
+                    )
+                } )
+                    .catch( () => {
+                        Alert(
+                            'Error',
+                            'Erro Updating Personal Data Sheet. Try Again',
+                            'error',
+                        )
+                    } )
+            }
+        )
+    }
+
+
     return (
         <div className="card-body card">
             <h5 className="bold mt-4 mb-4">
                 WORK EXPERIENCE
-                 <br />
+                <br />
                 <small className="text-muted" style={{ fontSize: '.8rem' }}> (INCLUDE PRIVATE EMPLOYMENT. START FROM YOUR RECENT WORK) DESCRIPTION OF DUTIES SHOULD BE INDICATED IN THE ATTACHED WORK EXPERIENCE SHEET.</small>
             </h5>
-            <div className="col-md-6 mb-4 p-0 m-0">
-                <button className="btn btn-outline-dark d-flex aic jcc">
+            <div className="col-md-6 mb-4 p-0 m-0 d-flex">
+                <button
+                    onClick={() => {
+                        setaddForm( [ ...addForm, 1 ] );
+                    }} className="btn btn-outline-dark d-flex aic jcc">
                     <i className="fe fe-plus"></i>
                     <span> Add Work Experience</span>
                 </button>
+                <button
+                    type="button"
+                    onClick={() => {
+                        setaddForm( [ 1 ] );
+                    }}
+                    className="btn btn-outline-danger d-flex aic jcc ml-3">
+                    <i className="fe fe-plus"></i>
+                    <span> Reset</span>
+                </button>
             </div>
-            <h6 className="bold mt-4 mb-4 text-info">WORK EXPERIENCE 1</h6>
-            <div className="row mb-4">
-                <div className="col-md-2 mb-4">
-                    <label htmlFor="">FROM</label>
-                    <input type="date" className="form-control" />
-                </div>
-                <div className="col-md-2 mb-4">
-                    <label htmlFor="">TO</label>
-                    <input type="date" className="form-control" />
-                </div>
-                <div className="col-md-8 mb-4">
-                    <label htmlFor="">POSITION TITLE (Write in full / Do not abbreviate)</label>
-                    <input type="text" className="form-control" />
-                </div>  <div className="col-md-12 mb-4">
-                    <label htmlFor="">DEPARTMENT / AGENCY / OFFICE / COMPANY (Write in full / Do not abbreviate)</label>
-                    <input type="text" className="form-control" />
-                </div>
-                <div className="col-md-12 mb-4">
-                    <label htmlFor="">SALARY/ JOB/ PAY GRADE (if  applicable) & STEP (Format "00-0") / INCREMENT</label>
-                    <input type="text" className="form-control" />
-                </div>
-                <div className="col-md-6 mb-4">
-                    <label htmlFor=""> STATUS OF APPOINTMENT</label>
-                    <input type="text" className="form-control" />
-                </div>
-                <div className="col-md-6 mb-4">
-                    <label htmlFor="">GOV'T SERVICE(Y/ N)</label>
-                    <input type="text" className="form-control" />
-                </div>
-            </div>
+
+            {
+                addForm.map( ( value, index ) => (
+                    <div key={index} className={`data${ index }`}>
+                        <h6 className="bold mt-4 mb-4 text-info">WORK EXPERIENCE {index + 1}</h6>
+                        <div className="row mb-4">
+                            <div className="col-md-2 mb-4">
+                                <label htmlFor="">FROM</label>
+                                <input id={`From${ index }`} type="date" className="form-control" />
+                            </div>
+                            <div className="col-md-2 mb-4">
+                                <label htmlFor="">TO</label>
+                                <input id={`To${ index }`} type="date" className="form-control" />
+                            </div>
+                            <div className="col-md-8 mb-4">
+                                <label htmlFor="">POSITION TITLE (Write in full / Do not abbreviate)</label>
+                                <input id={`Position${ index }`} type="text" className="form-control" />
+                            </div>
+                            <div className="col-md-12 mb-4">
+                                <label htmlFor="">DEPARTMENT / AGENCY / OFFICE / COMPANY (Write in full / Do not abbreviate)</label>
+                                <input id={`Department${ index }`} type="text" className="form-control" />
+                            </div>
+                            <div className="col-md-12 mb-4">
+                                <label htmlFor="">SALARY/ JOB/ PAY GRADE (if  applicable) & STEP (Format "00-0") / INCREMENT</label>
+                                <input id={`Salary${ index }`} type="text" className="form-control" />
+                            </div>
+                            <div className="col-md-6 mb-4">
+                                <label htmlFor=""> STATUS OF APPOINTMENT</label>
+                                <input id={`Appointment${ index }`} type="text" className="form-control" />
+                            </div>
+                            <div className="col-md-6 mb-4">
+                                <label htmlFor="">GOV'T SERVICE(Y/ N)</label>
+                                <select id={`Government${ index }`} className="form-control" >
+                                    <option >Yes</option>
+                                    <option >No</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                ) )
+            }
+
             <div className="mt-2 mb-4 d-flex aij jcc">
-                <button onClick={() => {
-                    Fire(
-                        'Update Personal Data Sheet?',
-                        ' I declare under oath that I have personally accomplished this Personal Data Sheet which is a true, correct and complete statement pursuant to the provisions of pertinent laws, rules and regulations of the Republic of the Philippines. I authorize the agency head/authorized representative to verify/validate the contents stated herein.I agree that any misrepresentation made in this document and its attachments shall cause the filing of administrative/criminal case/s against me.',
-                        'info',
-                        () => { }
-                    )
-                }} className="btn btn-outline-primary">Update Work Experience</button>
+                <button onClick={() => [
+                    submit()
+                ]} className="btn btn-outline-primary">Update Work Experience</button>
             </div>
         </div>
     )
