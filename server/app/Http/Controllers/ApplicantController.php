6ad67a51bc28832931881023291f48a7b6d6c54b
Jamel Yassin <jamelyassin84@gmail.com>
2021-06-23 14:02:01 +0800

    schedule email

--- server/app/Http/Controllers/ApplicantController.php
+++ server/app/Http/Controllers/ApplicantController.php
@@ -5,12 +5,14 @@ namespace App\Http\Controllers;
 use App\Models\Applicant;
 use App\Http\Controllers\Controller;
 use App\Models\User;
-use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Mail;
 
+$global_email = '';
+    
 class ApplicantController extends Controller
 {
+    
     public function index()
     { 
         return Applicant::with('user')->with('attachments')->get();
@@ -29,13 +31,13 @@ class ApplicantController extends Controller
         $applicant = Applicant::with('user')->where('user_id',$id)->first();
         $applicant->Schedule = $request->input('Schedule');
         $applicant->save();
+        $GLOBALS['global_email'] = $applicant->user->Email;
         Mail::send('emails.schedule',[
             'applicant' =>$applicant ,
             'schedule' => $request->input('on')
         ], function ($message) {
-            $email = 'jyassin84@gmail.com';
             $message->from(env('APP_EMAIL'), env('APP_NAME'));
-            $message->to( $email )->subject('You have shortlisted for an interview on Iloilo State College of Fisheries');
+            $message->to( $GLOBALS['global_email'] )->subject('You have shortlisted for an interview on Iloilo State College of Fisheries');
         });
         return $applicant;
     }
