4619eaae239ec1dafce8d43d9f5bd17d8b75790e
Jamel Yassin <jamelyassin84@gmail.com>
2021-06-26 17:28:52 +0800

    scholarship and seminars UI

--- web/src/views/HRD/Scholarships/Scholarship.tsx
+++ web/src/views/HRD/Scholarships/Scholarship.tsx
@@ -1,9 +1,117 @@
 import React from 'react'
+import { noData } from '../../../components/Alerts/Alert'
+import { Auth } from '../../../services/auth.service'
+import SupervisorPlaceholder from '../../Supervisor/SupervisorPlaceholder'
+
 
 export default function Scholarship() {
+
+    const [ scholarships, setscholarships ] = React.useState( [] )
+    const [ filteredData, setFilteredData ] = React.useState( [] )
+    const [ fetched, setfetched ]: any = React.useState( false )
+    const user: any = localStorage.getItem( 'user' )
+    const id = JSON.parse( user ).id
+    const userData: any = localStorage.getItem( 'user' )
+    const type = JSON.parse( userData ).Type
+
+
+    React.useEffect( () => {
+        getScholarships()
+    }, [] )
+
+    async function getScholarships() {
+        const auth = new Auth( 'scholarships' );
+        auth.fetch( {} ).then( ( data: any ) => {
+            setscholarships( data )
+            setFilteredData( data )
+            setfetched( true )
+        } )
+    }
+
+    const search = ( e: any ) => {
+        const keyword = e.target.value
+        keyword === '' ?
+            setFilteredData( scholarships ) :
+            setFilteredData( scholarships.filter(
+                ( data: any ) =>
+                    data.Title.toLowerCase().includes( keyword )
+            ) )
+    }
+
+    const renderData = () => {
+        noData()
+        if ( scholarships.length === 0 ) {
+            return <tr>
+                <td className="text-center text-muted" colSpan={4}>No sholarships yet...</td>
+            </tr>
+        }
+    }
+
+
     return (
         <div>
-            Scholarship
+            <div className="col-md-12 my-4">
+                <h2 className=" mb-1">Scholarships</h2>
+                <p className="mb-3 text-muted">Displaying List of Sholarship in descending order</p>
+                <div className="card shadow">
+                    <div className="card-body">
+                        <div className="toolbar">
+                            <div className="form-row">
+                                <div className="form-group col-auto">
+                                    <label className="sr-only">Search</label>
+                                    <input onChange={( e ) => {
+                                        // search( e )
+                                    }} type="text" className="form-control" id="search1" placeholder="Search" />
+                                </div>
+                            </div>
+                        </div>
+                        <table className="table">
+
+                            <thead>
+                                <tr>
+                                    <th className="text-danger">#</th>
+                                    <th className="text-danger"></th>
+                                    <th className="text-danger">Title of Scholarship</th>
+                                    <th className="text-danger">Availability</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {/* <SupervisorPlaceholder show={!fetched} /> */}
+                                {
+                                    renderData()
+                                }
+                                <tr>
+                                    <td>1</td>
+                                    <td>
+                                        <div className="circle circle-sm bg-light">
+                                            <i className="fe fe-columns text-muted"></i>
+                                        </div>
+                                        <span className="dot dot-md bg-danger mr-1"></span>
+                                    </td>
+                                    <td>K-12</td>
+                                    <td>
+                                        <span className="badge badge-danger">Unavailable</span>
+                                    </td>
+                                    <th>
+                                        <button style={{ display: type === 'Admin' ? '' : 'none' }} className="btn fe fe-24 fe-chevron-down" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            <span className="text-muted sr-only">Action</span>
+                                        </button>
+                                        <div className="dropdown-menu dropdown-menu-right">
+                                            <button
+                                                onClick={() => {
+
+                                                }}
+                                                role="butoon" className="dropdown-item" >Remove Scholarship</button>
+                                        </div>
+                                    </th>
+                                </tr>
+                            </tbody>
+                        </table>
+
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- web/src/views/HRD/Upcomming-seminars/NewSeminarsAttended.tsx
+++ web/src/views/HRD/Upcomming-seminars/NewSeminarsAttended.tsx
@@ -1,9 +1,91 @@
 import React from 'react'
 
 export default function NewSeminarsAttended() {
+
+    const [ dateFilter, setdateFilter ] = React.useState( '' )
+    const [ years, setyears ] = React.useState( [] )
+
+    React.useEffect( () => {
+        loopYears()
+    }, [] )
+
+    const loopYears = () => {
+        let yearArray: any = []
+        for ( let i = 2019; i < 2051; i++ ) {
+            yearArray.push( i )
+        }
+        setyears( yearArray )
+    }
+
+    React.useEffect( () => {
+
+    }, [ dateFilter ] )
+
     return (
-        <div>
-            NewSeminarsAttended
+        <div className="row justify-content-center">
+            <div className="col-12 col-lg-10 col-xl-8">
+                <div className="col-md-12 mb-4">
+                    <div className="card timeline">
+                        <div className="card-header d-flex">
+                            <strong className="card-title">Seminars  </strong>
+                            <div className="ml-auto ">
+                                <button className="btn btn-dark" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Filter</button>
+                                <div className="dropdown-menu dropdown-menu-right" >
+                                    {
+                                        years.map( ( year, index ) => (
+                                            <button role="butoon" className="dropdown-item" >{year}</button>
+
+                                        ) )
+                                    }
+                                </div>
+                            </div>
+
+                        </div>
+                        <div className="card-body row">
+                            <>
+                                <div className="pb-3 timeline-item item-success col-md-12">
+                                    <div className="pl-5 w-100">
+                                        <div className="d-flex w-100 row">
+                                            <div className="col-md-1 d-flex  jcc">
+                                                <div className="avatar avatar-lg">
+                                                    <img src="seminar.user.Avatar" alt="..." className="avatar-img rounded-circle" />
+                                                </div>
+                                            </div>
+                                            <div className="col-md-6">
+                                                <strong>@seminar.Title  </strong>
+                                                <br />
+                                                <small>Attended by: seminar.user.First seminar.user.Middle seminar.user.Last </small>
+                                                <br />
+                                                <span className="text-info h5  ">
+                                                    seminar.SponsoredBy
+                                                </span>
+                                                <br />
+                                                <small >
+                                                    Total hrs:  <small className=''>seminar.Hours hrs</small>
+                                                </small>
+                                            </div>
+                                            <div className="col-md-4 text-right">
+                                                <small >
+                                                    From:  <small className='text-danger'>toDate( seminar.From )</small>
+                                                </small>
+                                                <br />
+                                                <small >
+                                                    To:  <small className='text-danger'>toDate( seminar.To )</small>
+                                                </small>
+                                                <br />
+                                                <span className="badge badge-danger">
+                                                    seminar.Type
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
